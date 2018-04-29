2e402dfd938bc9e2fd09532c2f8f3dfbc16e36d4
terrycojones <terry@jon.es>
2018-04-29 14:28:53 +0100

    First commit.

---
+++ 01-collect/collect.sh
@@ -0,0 +1,93 @@
+#!/bin/bash -e
+
+. ../common.sh
+
+log=$sampleLogFile
+out=$(sampleName)-hbv.fastq
+
+logStepStart $log
+logTaskToSlurmOutput collect $log
+
+function skip()
+{
+    echo "  Skipping at $(date)" >> $log
+}
+
+function collect()
+{
+    # Combine and de-duplicate (by id) FASTQ for HBV for this sample from
+    # the DIAMOND and BWA pipelines.
+
+    echo "  FASTQ collection started at `date`" >> $log
+
+    sample=$(sampleName)
+    sampleIndex=$(egrep " $sample\$" ../../../refseq-diamond/samples.index | cut -f1 -d' ')
+    hbvIndex=$(egrep ' Hepatitis B virus$' ../../../refseq-diamond/pathogens.index | cut -f1 -d' ')
+    echo "    Sample $sample index is $sampleIndex" >> $log
+    echo "    HBV index is $hbvIndex" >> $log
+
+    countFile=$sample-hbv.read-count
+    rmFileAndLink $out $countFile
+
+    fastq=
+
+    f=../../../refseq-diamond/$sample/04-panel/out/pathogen-$hbvIndex-sample-$sampleIndex.fastq
+    if [ -f $f ]
+    then
+        echo "    DIAMOND HBV FASTQ for sample $sample found in $f" >> $log
+        fastq="$fastq $f"
+    else
+        echo "    DIAMOND HBV FASTQ for sample $sample file $f does not exist" >> $log
+    fi
+
+    f=../../../hbv-bwa/$sample/04-collect/$sample-hbv.fastq
+    if [ -f $f ]
+    then
+        echo "    BWA HBV FASTQ for sample $sample found in $f" >> $log
+        fastq="$fastq $f"
+    else
+        echo "    BWA HBV FASTQ for sample $sample file $f does not exist" >> $log
+    fi
+
+    if [ -n "$fastq" ]
+    then
+        echo "    Combining HBV FASTQ for sample $sample" >> $log
+        cat $fastq | filter-fasta.py --quiet --removeDuplicatesById --fastq > $out
+        # The following count could have been extracted from the stderr of
+        # the previous command if we hadn't used --quiet, but that's a bit
+        # ugly and this count will be very quick as we only have at most a
+        # few thousand HBV reads for a sample.
+        fasta-count.py --fastq < $out > $countFile
+    else
+        echo "    No HBV FASTQ for sample $sample" >> $log
+    fi
+
+    echo "  FASTQ collection stopped at `date`" >> $log
+}
+
+
+if [ $SP_SIMULATE = "1" ]
+then
+    echo "  This is a simulation." >> $log
+else
+    echo "  This is not a simulation." >> $log
+    if [ $SP_SKIP = "1" ]
+    then
+        echo "  $(stepName) is being skipped on this run." >> $log
+        skip
+    elif [ -f $out ]
+    then
+        if [ $SP_FORCE = "1" ]
+        then
+            echo "  Pre-existing output file $out exists, but --force was used. Overwriting." >> $log
+            collect
+        else
+            echo "  Will not overwrite pre-existing output file $out. Use --force to make me." >> $log
+        fi
+    else
+        echo "  Pre-existing output file $out does not exist." >> $log
+        collect
+    fi
+fi
+
+logStepStop $log
